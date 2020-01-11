43a0287aef08aa907d3f54096fd8d9a316746a0d
ncev <nicolaseven76@gmail.com>
2020-01-11 13:54:47 +0100

    update on test

--- microUser/apiUser/tests.py
+++ microUser/apiUser/tests.py
@@ -3,18 +3,18 @@ from .tools.getURL import getJSON
 
 class TestGetModelMethod(TestCase):
     def setUp(self):
-        # Setup run before every test method.
-        # 
+        self.return_value =  {
+            'WRONG': {'error': 'requests'},
+            'BAD': {'error': 'json parse'},
+            'RIGHT': ['all', 'exist/username/{username}', 'exist/token/{access_token}']
+        }
         pass
 
     def test_wrong_url(self):
-        return_value = {'error': 'requests'}
-        self.assertTrue(getJSON('WRONGURL') == return_value) # dans le cas d'une URL incorrect
+        self.assertTrue(getJSON('WRONGURL') == self.return_value['WRONG']) # dans le cas d'une URL incorrect
 
     def test_bad_url(self):
-        return_value = {'error': 'json parse'}
-        self.assertTrue(getJSON('https://www.djangoproject.com/') == return_value) # dans le cas d'une mauvaise url
+        self.assertTrue(getJSON('https://www.djangoproject.com/') == self.return_value['BAD']) # dans le cas d'une mauvaise url
 
     def test__url(self):
-        return_value = ['all', 'exist/username/{username}', 'exist/token/{access_token}']
-        self.assertTrue(getJSON('http://127.0.0.1:3000/api') == return_value) # dans le cas d'une réussite
+        self.assertTrue(getJSON('http://127.0.0.1:3000/api') == self.return_value['RIGHT']) # dans le cas d'une réussite
