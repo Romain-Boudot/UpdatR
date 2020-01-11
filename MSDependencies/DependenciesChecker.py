361587c0c0fb4b2d6004dc7405c0a99ca1890d3a
BaptisteMagoni <34521668+BaptisteMagoni@users.noreply.github.com>
2020-01-11 17:40:56 +0100

    Mise à jour Rabbit

--- MSDependencies/DependenciesChecker.py
+++ MSDependencies/DependenciesChecker.py
@@ -3,10 +3,8 @@
 
 from RabbitMQWrapper import RabbitMQWrapper
 from ReportHandler.ChecksDependencies import ChecksDependencies
+
 if __name__ == "__main__":
-    """url = "amqp://guest:Romain01@app.updatr.tech"
+    url = "amqp://guest:Romain01@app.updatr.tech"
     rabbit = RabbitMQWrapper(url=url) #Instanciation pour lire les queues
-    rabbit.listen('nico') #Lit les queues"""
-    url = 'https://github.com/BaptisteMagoni/app-questionnaire-vue.git'
-    check = ChecksDependencies(url)  # Instanciation pour récupérer les dépendences du projet et fonction de sont chemin ou url
-    check.start()
+    rabbit.listen('url_git') #Lit les queues
