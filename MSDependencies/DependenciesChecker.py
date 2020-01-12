0a167c87c86dead391cc37616d42d59d4e94c0fb
BaptisteMagoni <34521668+BaptisteMagoni@users.noreply.github.com>
2020-01-12 17:53:37 +0100

    Mise à jour de la commande Outdated

--- MSDependencies/DependenciesChecker.py
+++ MSDependencies/DependenciesChecker.py
@@ -5,6 +5,8 @@ from RabbitMQWrapper import RabbitMQWrapper
 from ReportHandler.ChecksDependencies import ChecksDependencies
 
 if __name__ == "__main__":
-    url = "amqp://guest:Romain01@app.updatr.tech"
+    """url = "amqp://guest:Romain01@app.updatr.tech"
     rabbit = RabbitMQWrapper(url=url) #Instanciation pour lire les queues
-    rabbit.listen('url_git') #Lit les queues
+    rabbit.listen('url_git') #Lit les queues"""
+    check = ChecksDependencies(path="https://github.com/BaptisteMagoni/app-questionnaire-vue.git")
+    check.start()
