4c7e9acad6e4ecd791d36cc670c46e658260c706
BaptisteMagoni <34521668+BaptisteMagoni@users.noreply.github.com>
2020-01-12 19:55:39 +0100

    Mise à jour rabbitMQ

--- MSDependencies/DependenciesChecker.py
+++ MSDependencies/DependenciesChecker.py
@@ -5,8 +5,10 @@ from RabbitMQWrapper import RabbitMQWrapper
 from ReportHandler.ChecksDependencies import ChecksDependencies
 
 if __name__ == "__main__":
-    """url = "amqp://guest:Romain01@app.updatr.tech"
-    rabbit = RabbitMQWrapper(url=url) #Instanciation pour lire les queues
-    rabbit.listen('url_git') #Lit les queues"""
-    check = ChecksDependencies(path="https://github.com/BaptisteMagoni/app-questionnaire-vue.git")
-    check.start()
+    while True:
+        url = "amqp://guest:Romain01@app.updatr.tech"
+        rabbit = RabbitMQWrapper(url=url) #Instanciation pour lire les queues
+        rabbit.listen('url_git') #Lit les queues
+
+    """check = ChecksDependencies(path="https://github.com/BaptisteMagoni/app-questionnaire-vue.git")
+    check.start()"""
--- MSDependencies/RabbitMQWrapper.py
+++ MSDependencies/RabbitMQWrapper.py
@@ -15,15 +15,17 @@ class RabbitMQWrapper:
 
     def listen(self, queue):
         def callback(ch, method, properties, body): #Body: json en str
-            resp = json.loads(body)
+            resp = json.loads(body.decode('utf8'))
             if resp["git_url"] is None:
                 resp["git_url"] = 'https://github.com/BaptisteMagoni/app-questionnaire-vue.git'
             print(resp['git_url'])
             check = ChecksDependencies(resp["git_url"])  # Instanciation pour récupérer les dépendences du projet et fonction de sont chemin ou url
             check.start()  # Permet de lancer la recherche
             resp["report"] = check.getReport()
+            print(resp["report"])
             if check.report.hasOutdatedPackage():
                 self.send(queue='alert', durable=True, body=json.dumps(check.getReport()), routing_key='alert')  # Envoie le rapport dans la queue alert
 
         self.channel = self.connection.channel()
         self.channel.basic_consume(queue=queue, on_message_callback=callback, auto_ack=True)
+        self.channel.start_consuming()
