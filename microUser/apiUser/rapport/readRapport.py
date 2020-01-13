20f1a29eaff34f7dd932d731e7c8a9be639de3eb
ncev <nicolaseven76@gmail.com>
2020-01-13 19:04:26 +0100

    update emit queue value

--- microUser/apiUser/rapport/readRapport.py
+++ microUser/apiUser/rapport/readRapport.py
@@ -4,7 +4,7 @@ from ..models import Rapport
 
 RABBIT = {
     'URL': 'amqp://guest:Romain01@app.updatr.tech',
-    'QUEUE_LISTEN': 'url_git',
+    'QUEUE_LISTEN': 'rapport',
     'QUEUE_EMIT': 'url_git'
 }
 
